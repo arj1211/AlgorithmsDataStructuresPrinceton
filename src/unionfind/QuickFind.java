9f33084dd9afd0adc946110720ab2c9aab2a1e86
arj1211 <thejoker1211@outlook.com>
2017-05-11 13:10:19 -0400

    i tried

--- src/unionfind/QuickFind.java
+++ src/unionfind/QuickFind.java
@@ -1,5 +1,4 @@
 /*
- * 
  * Uses int array of length N to store a union's "id".
  * p and q are connected if and only if they have the same id.
  * 
@@ -18,39 +17,48 @@
  * 3. when creating a union between p and q, 
  * 		id[i] = q , where i is any object connected 
  * 					to p (including p itself, so
- * 					id[p] = q )
- * 
+ * 					id[p] = q ) 
  */
 
-
 package unionfind;
+
+import java.util.Scanner;
 
 public class QuickFind {
 
 	private int[] id;
-	
-	public QuickFind(int N)
-	{
-		id = new int[N+1];
-		for (int i=1;i<=N;i++)
-		{
-			id[i]=i;
+
+	public QuickFind(int N) {
+		id = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			id[i] = i;
 		}
 	}
-	
-	void union(int p, int q)
-	{
-		if (!connected(p,q))
-		{
-			for (int i =1; i<=id.length; i++)
-			{
-				if (id[i]==p) id[i]=q;
+
+	void union(int p, int q) {
+		if (!connected(p, q)) {
+			for (int i = 1; i <= id.length; i++) {
+				if (id[i] == p)
+					id[i] = q;
 			}
 		}
 	}
+
+	boolean connected(int p, int q) {
+		return id[p] == id[q];
+	}
 	
-	boolean connected(int p, int q)
-	{
-		return id[p]==id[q];
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int N = Integer.parseInt(sc.nextLine());
+		QuickFind qf = new QuickFind(N);
+		String[] s = new String[2];
+		while(true)
+		{
+			s[0]=sc.nextLine().trim();
+			if (s[0].toLowerCase()=="x") break;
+			s=s[0].split(" ");
+			qf.union(Integer.parseInt(s[0]),Integer.parseInt(s[1]));
+		}
 	}
 }
