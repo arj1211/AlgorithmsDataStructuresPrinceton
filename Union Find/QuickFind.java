71af7eb3d161adce19781eced9c1cd654f3557b2
arj1211 <thejoker1211@outlook.com>
2017-05-11 22:45:47 -0400

    fixed logic

--- Union Find/QuickFind.java
+++ Union Find/QuickFind.java
@@ -15,48 +15,60 @@
  * 2. if id[p]==id[q] then p and q connected, else not connected
  * 
  * 3. when creating a union between p and q, 
- * 		id[i] = q , where i is any object connected 
+ * 		id[i] = id[q] , where i is any object connected 
  * 					to p (including p itself, so
- * 					id[p] = q ) 
+ * 					id[p] = id[q] ) 
  */
 
 import java.util.Scanner;
 
-public class QuickFind {
+public class QuickFind 
+{
 
-	private int[] id;
+	private static int[] id;
 
-	public QuickFind(int N) {
-		id = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
+	public QuickFind(int N) 
+	{
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
 			id[i] = i;
 		}
 	}
 
-	void union(int p, int q) {
-		if (!connected(p, q)) {
-			for (int i = 1; i <= id.length; i++) {
-				if (id[i] == p)
-					id[i] = q;
-			}
+	void union(int p, int q) 
+	{
+		for (int i=0;i<id.length;i++)
+		{
+			if (id[i]==id[p]) id[i]=id[q];
 		}
 	}
 
-	boolean connected(int p, int q) {
+	boolean connected(int p, int q) 
+	{
 		return id[p] == id[q];
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner sc = new Scanner(System.in);
 		int N = Integer.parseInt(sc.nextLine());
 		QuickFind qf = new QuickFind(N);
-		String[] s = new String[2];
-		while (true) {
-			s[0] = sc.nextLine().trim();
-			if (s[0].toLowerCase() == "x")
-				break;
-			s = s[0].split(" ");
-			qf.union(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+		String s;
+
+		while(true)
+		{
+			s = sc.nextLine().trim();
+			if (s.toLowerCase().equals("x")) break;
+
+			int p = Integer.parseInt(s.charAt(0)+"");
+			int q = Integer.parseInt(s.charAt(2)+"");
+
+			qf.union(p,q);
+		}
+
+		for (int i=0;i<id.length;i++)
+		{
+			System.out.println(i+" "+id[i]);
 		}
 	}
 }
